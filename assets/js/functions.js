ffadb06826194c180e8c32243518cbb928465080
Masami Okada <mokada42991@gmail.com>
2017-10-23 23:04:04 +0200

    Hero background slideshow, fixed header

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,3 +1,21 @@
 $(document).ready(function() {
-    
+    console.log("yes");
+
+    var currentPhoto = 0;
+
+    $('.background').eq(currentPhoto).addClass('current-photo').css('display','block');
+
+    function backgroundSlideshow() {
+        looper = setInterval(function() {
+            $('.current-photo').removeClass('current-photo').fadeOut(2000, 'easeInOutQuad');
+
+            if (currentPhoto == 2) currentPhoto = 0;
+            else currentPhoto++;
+
+            $('.background').eq(currentPhoto).addClass('current-photo').css('display','none').fadeIn(2000, 'easeInOutQuad');
+
+        }, 6000);
+    }
+
+    backgroundSlideshow();
 });
