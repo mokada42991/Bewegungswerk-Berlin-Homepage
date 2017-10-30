1dc1d85c68fdb3f8d8a5e32548966def59503ce5
Masami Okada <mokada42991@gmail.com>
2017-10-30 15:47:25 +0100

    Therapien section, new hero images

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,13 +1,27 @@
 $(document).ready(function() {
+    // Detect if user is on mobile and change the facebook link to the appropriate version.
+    function detectMobile() {
+        if( navigator.userAgent.match(/Android/i)
+            || navigator.userAgent.match(/webOS/i)
+            || navigator.userAgent.match(/iPhone/i)
+            || navigator.userAgent.match(/iPad/i)
+            || navigator.userAgent.match(/iPod/i)
+            || navigator.userAgent.match(/BlackBerry/i)
+            || navigator.userAgent.match(/Windows Phone/i)
+        ) {
+            $(".facebook-link").attr("href", "fb://profile/457541911052222");
+        }
+    };
+
     // Hero background slideshow
     var currentPhoto = 0;
     $('.background').eq(currentPhoto).addClass('current-photo').css('display','block');
     function backgroundSlideshow() {
         looper = setInterval(function() {
-            $('.current-photo').removeClass('current-photo').fadeOut(2000, 'easeInOutQuad');
+            $('.current-photo').removeClass('current-photo').fadeOut(2000);
             if (currentPhoto == 2) currentPhoto = 0;
             else currentPhoto++;
-            $('.background').eq(currentPhoto).addClass('current-photo').css('display','none').fadeIn(2000, 'easeInOutQuad');
+            $('.background').eq(currentPhoto).addClass('current-photo').css('display','none').fadeIn(2000);
         }, 6000);
     }
 
@@ -25,20 +39,15 @@ $(document).ready(function() {
         }, 1700, "easeInOutExpo");
     });
 
-    // Detect if user is on mobile and change the facebook link to the appropriate version.
-    function detectMobile() {
-        if( navigator.userAgent.match(/Android/i)
-            || navigator.userAgent.match(/webOS/i)
-            || navigator.userAgent.match(/iPhone/i)
-            || navigator.userAgent.match(/iPad/i)
-            || navigator.userAgent.match(/iPod/i)
-            || navigator.userAgent.match(/BlackBerry/i)
-            || navigator.userAgent.match(/Windows Phone/i)
-        ) {
-            $(".facebook-link").attr("href", "fb://profile/457541911052222");
-        }
-    };
+    // Show and hide therapie info button functions
+    $(".open-info").on("click", function() {
+        $(this).next().fadeIn("slow");
+        $(this).next().css("display", "flex");
+    });
+    $(".close-info").on("click", function() {
+        $(this).parent().fadeOut("slow");
+    });
 
     detectMobile();
-    //backgroundSlideshow();
+    backgroundSlideshow();
 });
